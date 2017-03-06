307896ae47348fa2cf66623c207a3409c8609f16
sharebaan <sharebaan@gmail.com>
2017-03-06 14:21:05 +0200

    cache checker final

--- app/Console/Commands/UpdateCache.php
+++ app/Console/Commands/UpdateCache.php
@@ -48,28 +48,53 @@ class UpdateCache extends Command
 					
 					$dep = PackagesSearchCached::where('id',$v->id_package_search)->first();
 					
+					
+					
 					$d = explode('-',$dep->departure_date);
 					$n = explode('-',Carbon::now()->toDateString());
 					
-					$date = Carbon::create($d[0],$d[1],$d[2],0,0,0);
-					$now = Carbon::create($n[0],$n[1],$n[2],0,0,0);
+					$date = Carbon::create($d[0],$d[1],$d[2],0);
+					$now = Carbon::create($n[0],$n[1],$n[2],0);
 				
 					if($date->gte($now) == false){continue;}
 				
+					$checkCP = CachedPrice::where([
+						'id_package'=>$v->id_package,
+						'id_room_category'=>$v->id_room_category,
+						'id_meal_plan'=>$v->id_meal_plan,
+						'departure_date'=>$dep->departure_date,
+						'gross'=>$v->gross,
+						'tax'=>$v->tax,
+						'soap_client'=>$v->soap_client
+					])->first();
+					
+					$validTo = $date->addDays($dep->duration);
+					
+					$checkPS = PriceSet::where([
+						'id'=>$checkCP->id_price_set,
+						'valid_from'=>$dep->departure_date,
+						'valid_to'=>$validTo,
+					])->first();
+				
+					if($checkPS == null){continue;}
+				
+					$latestPS = PriceSet::orderBy('id','DESC')->first()->id;
+				
 					$ps = new PriceSet;
-					$ps->id = 2000;
+					$ps->id = $latestPS + 1;
 					$ps->valid_from = $dep->departure_date;
-					$ps->valid_to = $dep->departure_date;
+					$ps->valid_to = $validTo;
 					$ps->soap_client = $v->soap_client;
 					$ps->label = 'Search';
 					$ps->description = 'Search';
 					$ps->is_local = 0;
 					$ps->save();
 					
+					
 					$c = new CachedPrice;
 					$c->id_package = $v->id_package;
 					$c->id_room_category = $v->id_room_category;
-					$c->id_price_set = 2000;
+					$c->id_price_set = $latestPS + 1;
 					$c->id_meal_plan = $v->id_meal_plan;
 					$c->departure_date = $dep->departure_date;
 					$c->gross = $v->gross;
@@ -78,6 +103,10 @@ class UpdateCache extends Command
 					$c->soap_client = $v->soap_client;
 					$c->save();
 					
+					
+					
 				}
+				
+				$this->comment('Update finished.');
     }
 }
